032064bf93f88ba2681fc0745a42bdbd35ef56a8
AvashDahal <102864547+AvashDahal@users.noreply.github.com>
2024-01-19 22:12:36 +0545

    new commit

---
+++ backend/models/userModel.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+const userSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 3,
+        maxLength: 20
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 3,
+        maxLength: 20
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        minLength: 3,
+        maxLength: 20
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 3,
+        maxLength: 20
+    },
+    location: {
+        type: String, // String ma store
+      },
+      role: {
+        type: String,
+        enum: ["user","admin"],
+        default:"user",
+      },
+      dateOfBirth:{
+        type:Date,
+        required:false,
+      
+      },
+      phoneNo:{
+        type:String,
+        required:false,
+      },
+      emergencyContact:{
+        type:String,
+        required:false,
+      },
+      gender:{
+        type:String,
+        required:false,
+        
+      },
+  
+      user_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+      },
+  
+      
+    },
+  
+    {
+      timestamps: true,
+    }
+  );
+  const User = mongoose.model("User",userSchema);
+  export default User;
